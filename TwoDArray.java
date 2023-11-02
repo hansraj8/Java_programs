3aa689478816b5559a08a0aa53e23131f49f6c9e
Hansraj sharma <105164338+hansraj8@users.noreply.github.com>
2023-11-02 17:56:01 +0530

    print 2d array

--- TwoDArray.java
+++ TwoDArray.java
@@ -2,26 +2,42 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwoDArray {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    public static int[][] arrayinput(){
+    Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
 
-        
-        //hard coded array
-        int[][] arr = {{1,2,3},{4,5,6},{7,8,9,10}};
-        System.out.println(arr);
-        System.out.println(Arrays.toString(arr[0]));
-        System.out.println(Arrays.toString(arr[1]));
-        
+        int[][] arr = new int[rows][cols];
+        return arr;
+    }
+    public static void print2DArray(int[][] arr){
 
-        //loop to iterate array
-        for(int i=0;i<arr.length;i++){
-            // go through the ith row
-          //  System.out.println(Arrays.toString(arr[i]));
-            for(int j =0;j< arr[i].length;j++){
-                System.out.print(arr[i][j]+" ");
+        int rows = arr.length;
+        for(int i=0; i < rows; i++){
+            // how to print the ith row
+            // System.out.println(Arrays.toString(arr[i]));
+            for(int j=0; j < arr[i].length ; j++){
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
-        } 
+        }
+    }
+    public static void main(String[] args) {
+
+        // //hard coded array
+        // int[][] arr = {{1,2,3},{4,5,6},{7,8,9,10}};
+        // System.out.println(arr);
+        // System.out.println(Arrays.toString(arr[0]));
+        // System.out.println(Arrays.toString(arr[1]));
+        
+
+        // //loop to iterate array
+        
+       
+                                                                                        
+        int[][] arr = arrayinput();
+        print2DArray(arr);
+
      }
 }
