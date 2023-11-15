677051566558c080b36e831a166e2113be001f57
Hansraj sharma <105164338+hansraj8@users.noreply.github.com>
2023-11-16 01:05:08 +0530

    practse

--- InbuiltFnStrings.java
+++ InbuiltFnStrings.java
@@ -1,6 +1,16 @@
 public class InbuiltFnStrings {
-    public static void main(String[] args) {
-        String s = "Hi my name is hansraj";
+
+    public static void filterFiles(){
+        String[] filenames = {"ing1.jpg","img2.png","img3.pdf","img4.pdf"};
+        for(String f: filenames){
+            if(f.endsWith(".pdf")){
+                System.out.println("pdf is availbale"+f);
+            }
+        }
+    }
+    public static void Inbuiltfns(){
+
+          String s = "Hi my name is hansraj";
         System.out.println(s.length());
         System.out.println(s.toUpperCase());
         System.out.println(s);
@@ -13,6 +23,20 @@ public class InbuiltFnStrings {
         System.out.println(state);
         state = state.replace("mango", "apple");
         System.out.println(state);
-
+    }
+    public static void printOcc(){
+        String doc = "this is a very large document of the java ,prsent in this file java can handle large number large";
+        String word = "large";
+        int idx = doc.indexOf(word);
+        while(idx!=-1){
+            System.out.println(idx);
+            idx = doc.indexOf(word, idx+1);
+            
+        }
+    }
+    public static void main(String[] args) {
+      
+         ///filterFiles();
+         printOcc();
     }
 }
